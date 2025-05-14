0116e9ba9b8ed75473b40bccf50a0542eb06e342
Aksingh098 <reheat8725@gmail.com>
2025-05-14 21:59:10 +0530

    Added subjectcard and imagresources

---
+++ app/src/main/java/com/example/studyapp/domain/model/Subject.kt
@@ -0,0 +1,18 @@
+package com.example.studyapp.domain.model
+
+import androidx.compose.ui.graphics.Color
+import com.example.studyapp.ui.theme.gradient1
+import com.example.studyapp.ui.theme.gradient2
+import com.example.studyapp.ui.theme.gradient3
+import com.example.studyapp.ui.theme.gradient4
+import com.example.studyapp.ui.theme.gradient5
+
+data class Subject(
+    val name:String,
+    val goalHours:Float,
+    val colors:List<Color>
+){
+    companion object{
+        val subjectCardColors = listOf(gradient1, gradient2, gradient3, gradient4, gradient5)
+    }
+}
