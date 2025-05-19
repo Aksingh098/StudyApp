729e27668e3bbcd1a5b37061f4c2f7d23ab565ad
Aksingh098 <reheat8725@gmail.com>
2025-05-19 23:10:25 +0530

    Adding tasklist and StudySession list

--- app/src/main/java/com/example/studyapp/domain/model/Tasks.kt
+++ app/src/main/java/com/example/studyapp/domain/model/Tasks.kt
@@ -1,10 +1,13 @@
 package com.example.studyapp.domain.model
 
 data class Task(
+
     val title: String,
     val description:String,
     val dueDate: Long,
     val priority: Int,
     val relatedToSubject: String,
-    val isComplete: Boolean
+    val isComplete: Boolean,
+    val taskSubjectId: Int,
+    val taskId: Int
 )
--- app/src/main/java/com/example/studyapp/ui/theme/dashboard/DashboardScreen.kt
+++ app/src/main/java/com/example/studyapp/ui/theme/dashboard/DashboardScreen.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
@@ -32,20 +33,115 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import com.example.studyapp.R
+import com.example.studyapp.domain.model.Session
 import com.example.studyapp.domain.model.Subject
+import com.example.studyapp.domain.model.Task
 import com.example.studyapp.ui.theme.components.CountCard
 import com.example.studyapp.ui.theme.components.SubjectCard
+import com.example.studyapp.ui.theme.components.studySessionList
 import com.example.studyapp.ui.theme.components.tasksList
 
 
 @Composable
 fun DashboardScreen() {
     val subjects = listOf(
-        Subject(name = "English", goalHours = 10f, colors = Subject.subjectCardColors[0]),
-        Subject(name = "Physics", goalHours = 10f, colors = Subject.subjectCardColors[1]),
-        Subject(name = "Maths", goalHours = 10f, colors = Subject.subjectCardColors[2]),
-        Subject(name = "Chemistry", goalHours = 10f, colors = Subject.subjectCardColors[3]),
-        Subject(name = "Hindi", goalHours = 10f, colors = Subject.subjectCardColors[4])
+        Subject(name = "English", goalHours = 10f, colors = Subject.subjectCardColors[0],subjectId = 0),
+        Subject(name = "Physics", goalHours = 10f, colors = Subject.subjectCardColors[1],subjectId = 0),
+        Subject(name = "Maths", goalHours = 10f, colors = Subject.subjectCardColors[2],subjectId = 0),
+        Subject(name = "Chemistry", goalHours = 10f, colors = Subject.subjectCardColors[3],subjectId = 0),
+        Subject(name = "Hindi", goalHours = 10f, colors = Subject.subjectCardColors[4],subjectId = 0)
+
+    )
+    val tasks = listOf(
+        Task(
+            title = "Prepare notes",
+            description = "",
+            dueDate = 0L,
+            priority = 0,
+            relatedToSubject = "",
+            isComplete = false,
+            taskId = 1,
+            taskSubjectId = 0
+        ) ,
+        Task(
+            title = "Do Homework",
+            description = "",
+            dueDate = 0L,
+            priority = 1,
+            relatedToSubject = "",
+            isComplete = false,
+            taskId = 1,
+            taskSubjectId = 0
+        ),
+        Task(
+            title = "become a gigachad",
+            description = "",
+            dueDate = 0L,
+            priority = 2,
+            relatedToSubject = "",
+            isComplete = true,
+            taskId = 1,
+            taskSubjectId = 0
+        ),
+        Task(
+            title = "200 pushups",
+            description = "",
+            dueDate = 0L,
+            priority = 3,
+            relatedToSubject = "",
+            isComplete = false,
+            taskId = 1,
+            taskSubjectId = 0
+        ),
+        Task(
+            title = "watch Avengers",
+            description = "",
+            dueDate = 0L,
+            priority = 5,
+            relatedToSubject = "",
+            isComplete = true,
+            taskId = 1,
+            taskSubjectId = 0
+        )
+
+
+    )
+    val sessions = listOf(
+        Session(
+            relatedToSubject = "English",
+            date = 0L,
+            duration = 2,
+            sessionSubjectId = 0,
+            sessionId = 0
+        ),
+        Session(
+            relatedToSubject = "Physics",
+            date = 0L,
+            duration = 2,
+            sessionSubjectId = 0,
+            sessionId = 0
+        ),
+        Session(
+            relatedToSubject = "Maths",
+            date = 0L,
+            duration = 2,
+            sessionSubjectId = 0,
+            sessionId = 0
+        ),
+        Session(
+            relatedToSubject = "Chemistry",
+            date = 0L,
+            duration = 2,
+            sessionSubjectId = 0,
+            sessionId = 0
+        ),
+        Session(
+            relatedToSubject = "Hindi",
+            date = 0L,
+            duration = 2,
+            sessionSubjectId = 0,
+            sessionId = 0
+        )
 
     )
     Scaffold(
@@ -87,7 +183,20 @@ fun DashboardScreen() {
                 sectionTitle = "UPCOMING TASKS",
                 emptyListText = "You don't have any upcoming tasks.\n  "+
                 "Click the + button in subject screen to add new task",
-                tasks = emptyList()
+                tasks = tasks,
+                onCheckBoxClick = {},
+                onTaskCardClick = {}
+            )
+            item { 
+                Spacer(modifier = Modifier.height(20.dp))
+            }
+            studySessionList(
+                sectionTitle = "RECENT STUDY SESSIONS",
+                emptyListText = "You don't have any recent study session \n "+
+                "Start a study session to begin recording your progress",
+                sessions =  sessions,
+                onDeleteIconClick = {}
+
             )
 
         }
